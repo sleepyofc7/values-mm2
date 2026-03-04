4e85716b80351547aec83df3c35b773d9e1b1dc6
sleepyofc7 <byteaura7750@gmail.com>
2026-03-04 14:11:53 -0300

    04/03/2026

---
+++ items.js
@@ -0,0 +1,54 @@
+// 🔥 AQUI VOCÊ ADICIONA SEUS ITENS
+// NÃO PRECISA MAIS ESCREVER HTML
+
+const items = [
+
+  {
+    name: "Nik's Scythe",
+    image: "Niks_Scythe.png",
+    value: "inestimável",
+    rarity: "Ancient",
+    origin: "Presente de Nikilis"
+  },
+
+  {
+    name: "Gingerscope",
+    image: "Gingerscope.png",
+    value: 18.000,
+    rarity: "Ancient",
+    origin: "Natal de 2023 (Tier 20)"
+  },
+
+  {
+    name: "TravelersAxe",
+    image: "TravelersAxe.png",
+    value: 7.850,
+    rarity: "Ancient",
+    origin: "Halloween 2023 (Tier 30)"
+  },
+
+   {
+    name: "Celestial",
+    image: "Celestial.png",
+    value: 1.775,
+    rarity: "Ancient",
+    origin: "Natal 2024 (Tier 20)"
+  },
+
+  {
+    name: "Vampires Axe",
+    image: "Vampires_Axe.png",
+    value: 925,
+    rarity: "Ancient",
+    origin: "Halloween 2024 (Tier 20)"
+  },
+
+  {
+    name: "Harvester",
+    image: "Harvester.png",
+    value: 400,
+    rarity: "Ancient",
+    origin: "Halloween 2021 (Tier 30)"
+  },
+
+];
