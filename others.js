7f1d17e895a360559fb7c0eefdf58b5a7a5a6926
sleepyofc7 <byteaura7750@gmail.com>
2026-03-05 10:06:14 -0300

    teste

---
+++ others.js
@@ -0,0 +1,19 @@
+const others=[
+
+{
+name:"Example Pet",
+value:10,
+rarity:"Pet",
+image:"pet1.png",
+folder:"others"
+},
+
+{
+name:"Example Set",
+value:50,
+rarity:"Set",
+image:"set1.png",
+folder:"others"
+}
+
+]
