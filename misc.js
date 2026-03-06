63ca4a28f6bbb1897883ea0c2ce3de07d39e6d24
sleepyofc7 <byteaura7750@gmail.com>
2026-03-06 13:38:49 -0300

    06/03/2026

--- misc.js
+++ misc.js
@@ -22,4 +22,125 @@ image:"Ultra_Papers.png",
 folder:"misc"
 },
 
+{
+name:"Box of Gold Papers",
+value: 4,
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Natal de 2016 (Comprado)",
+image:"Gold_Papers.png",
+folder:"misc"
+},
+
+{
+name:"Box of Blue Papers",
+value: 3,
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Natal de 2017 (Comprado)",
+image:"Blue_Papers.png",
+folder:"misc"
+},
+
+{
+name:"Box of Purple Papers",
+value: 3,
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Natal de 2016 (Comprado)",
+image:"Purple_Papers.png",
+folder:"misc"
+},
+
+{
+name:"Box of Red Papers",
+value: 3,
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Natal de 2016 (Comprado)",
+image:"Red_Papers.png",
+folder:"misc"
+},
+
+{
+name:"Box of Fertilizer",
+value: 2,
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Natal de 2018 (Comprado)",
+image:"Fert.png",
+folder:"misc"
+},
+
+{
+name:"Box of Green Papers",
+value: 2,
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Natal de 2016 (Comprado)",
+image:"Green_Papers.png",
+folder:"misc"
+},
+
+{
+name:"Gifts 2015",
+value: "x2 T1 Legendaries",
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Natal de 2015 (Moedas)",
+image:"Xmas_Gifts.png",
+folder:"misc"
+},
+
+{
+name:"Snowflake Key",
+value: "x2 T1 Legendaries",
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Natal de 2018 (Tiers)",
+image:"Snow_Key.png",
+folder:"misc"
+},
+
+{
+name:"Skeleton Key",
+value: "5",
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Halloween de 2018 (Tiers)",
+image:"Skeleton_Key.png",
+folder:"misc"
+},
+
+{
+name:"Candies (2016)",
+value: "x2 T1 Commons",
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Halloween de 2016 (Moedas)",
+image:"Candy_2016.png",
+folder:"misc"
+},
+
+{
+name:"Candies (2017)",
+value: "x1 T1 Commons",
+demand: "1",
+stability: "Estável",
+rarity:"Misc",
+origin: "Halloween de 2017 (Moedas)",
+image:"Candy_2017.png",
+folder:"misc"
+},
+
 ];
