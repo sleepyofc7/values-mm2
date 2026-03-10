81ba23462b82eb24e0b9500e71672d21b7d41652
sleepyofc7 <byteaura7750@gmail.com>
2026-03-10 12:01:59 -0300

    .

---
+++ pets.js
@@ -0,0 +1,15 @@
+// PETS
+const pets = [
+    
+{
+name: "Zombie Dog",
+image: "Zombie_Dog.png",
+value: "725",
+demand: "4",
+stability: "Valores Altíssimos",
+rarity: "Pet",
+origin: "Halloween 2019 (Tier 25)",
+folder:"pets"
+},
+
+];
