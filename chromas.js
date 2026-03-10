d97553df601962409927653dd076f7e54aa20956
sleepyofc7 <byteaura7750@gmail.com>
2026-03-10 11:28:32 -0300

    10/03/2026

---
+++ chromas.js
@@ -0,0 +1,13 @@
+const items = [
+
+  {
+    name: "C. Traveler's Gun",
+    image: "CTG.png",
+    value: "220.000",
+    demand: "9",
+    stability: "Indo Bem",
+    rarity: "Chroma",
+    origin: "Halloween 2023 (Unboxed)",
+    folder:"chroma"
+  },
+];
