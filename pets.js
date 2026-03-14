208c9c99f216ac3a90d6362d4a7beb371dc927d8
sleepyofc7 <byteaura7750@gmail.com>
2026-03-14 12:38:46 -0300

    .

--- pets.js
+++ pets.js
@@ -12,4 +12,412 @@ origin: "Halloween 2019 (Tier 25)",
 folder:"pets"
 },
 
+{
+name: "Elf (2019)",
+image: "Elf_2019.png",
+value: "200",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Natal 2019 (Tier 12)",
+folder:"pets"
+},
+
+{
+name: "Blue Pumpkin (2018)",
+image: "Blue_Pumpkin.png",
+value: "160",
+demand: "3",
+stability: "Indo Bem",
+rarity: "Pet",
+origin: "Halloween 2018 (Tier 18)",
+folder:"pets"
+},
+
+{
+name: "Red Pumpkin (2018)",
+image: "Red_Pumpkin.png",
+value: "100",
+demand: "3",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2018 (Tier 12)",
+folder:"pets"
+},
+
+{
+name: "Dogey",
+image: "Dogey.png",
+value: "90",
+demand: "3",
+stability: "Estável",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+
+{
+name: "Green Pumpkin (2018)",
+image: "Green_Pumpkin.png",
+value: "60",
+demand: "3",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2018 (Tier 9)",
+folder:"pets"
+},
+
+{
+name: "Black Cat",
+image: "Black_Cat.png",
+value: "60",
+demand: "3",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2019 (Tier 5)",
+folder:"pets"
+},
+
+{
+name: "Pumpkin (2017)",
+image: "Pumpkin_2017_2018.png",
+value: "25",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2017 (HW2017)",
+folder:"pets"
+},
+
+{
+name: "Piggy",
+image: "Piggy.png",
+value: "20",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+
+{
+name: "Mr. Reindeer",
+image: "Mr_Reindeer.png",
+value: "15",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Natal 2020 (Tier 10)",
+folder:"pets"
+},
+
+{
+name: "Fairy",
+image: "Fairy.png",
+value: "13",
+demand: "1",
+stability: "Estável",
+rarity: "Pet",
+origin: "Loja no jogo (Expirada)",
+folder:"pets"
+},
+
+{
+name: "Jetstream",
+image: "Jetstream.png",
+value: "13",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Loja no jogo (Expirada)",
+folder:"pets"
+},
+
+{
+name: "Nobledragon",
+image: "Nobledragon.png",
+value: "13",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+
+{
+name: "Seahorsey",
+image: "Seahorsey.png",
+value: "13",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+
+{
+name: "Chilly",
+image: "Chilly.png",
+value: "12",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+
+{
+name: "Green Pumpkin (2019)",
+image: "Green_Pumpkin_2019.png",
+value: "12",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Halloween 2019 (Tier 20)",
+folder:"pets"
+},
+
+{
+name: "Pengy",
+image: "Pengy.png",
+value: "12",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+
+{
+name: "Purple Pumpkin (2018)",
+image: "Purple_Pumpkin.png",
+value: "12",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Halloween 2018 (Tier 35)",
+folder:"pets"
+},
+
+{
+name: "Red Pumpkin (2019)",
+image: "Red_Pumpkin_2019.png",
+value: "12",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Halloween 2019 (Tier 28)",
+folder:"pets"
+},
+
+{
+name: "Rudolph",
+image: "Rudolph.png",
+value: "12",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Natal 2019 (Tier 21)",
+folder:"pets"
+},
+
+{
+name: "Vampire Bat",
+image: "Vampire_Bat.png",
+value: "12",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Halloween 2019 (Tier 31)",
+folder:"pets"
+},
+
+{
+name: "<3",
+image: "Heart.png",
+value: "10",
+demand: "1",
+stability: "Estável",
+rarity: "Pet",
+origin: "Valentine's 2016",
+folder:"pets"
+},
+
+{
+name: "Eyeball",
+image: "Eyeball.png",
+value: "10",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Loja no jogo (Expirada)",
+folder:"pets"
+},
+
+{
+name: "Reindeer",
+image: "Reindeer.png",
+value: "10",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Natal 2016 (Comprado)",
+folder:"pets"
+},
+
+{
+name: "Tankie",
+image: "Tankie.png",
+value: "10",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+// TIER 3
+{
+name: "Elf",
+image: "Elf.png",
+value: "8",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Natal 2016 (Comprado)",
+folder:"pets"
+},
+
+{
+name: "Red Pumpkin (2020)",
+image: "Red_Pumpkin_2020.png",
+value: "8",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2020 (Tier 26)",
+folder:"pets"
+},
+
+{
+name: "Overseer_Eye",
+image: "Overseer_Eye.png",
+value: "8",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Loja no jogo (Expirada)",
+folder:"pets"
+},
+
+{
+name: "Red Pumpkin (2021)",
+image: "Red_Pumpkin_2021.png",
+value: "7",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2021 (Tier 27)",
+folder:"pets"
+},
+
+{
+name: "Skully",
+image: "Skully.png",
+value: "7",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2021 (Tier 21)",
+folder:"pets"
+},
+
+{
+name: "Green Pumpkin (2020)",
+image: "Green_Pumpkin_2020.png",
+value: "5",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2020 (Tier 18)",
+folder:"pets"
+},
+
+{
+name: "Green Pumpkin (2021)",
+image: "Green_Pumpkin_2021.png",
+value: "5",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2021 (Tier 17)",
+folder:"pets"
+},
+
+{
+name: "Mechbug",
+image: "Mechbug.png",
+value: "5",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+
+{
+name: "UFO",
+image: "UFO.png",
+value: "5",
+demand: "1",
+stability: "item mal pago",
+rarity: "Pet",
+origin: "Classic Pet Box",
+folder:"pets"
+},
+
+{
+name: "Blue Pumpkin (2020)",
+image: "Blue_Pumpkin_2020.png",
+value: "3",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2020 (Tier 12)",
+folder:"pets"
+},
+
+{
+name: "Blue Pumpkin (2019)",
+image: "Blue_Pumpkin_2019.png",
+value: "2",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2019 (Tier 9)",
+folder:"pets"
+},
+
+{
+name: "Blue Pumpkin (2019)",
+image: "Blue_Pumpkin_2019.png",
+value: "2",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2019 (Tier 9)",
+folder:"pets"
+},
+
+{
+name: "Blue Pumpkin (2019)",
+image: "Blue_Pumpkin_2019.png",
+value: "2",
+demand: "2",
+stability: "Estável",
+rarity: "Pet",
+origin: "Halloween 2019 (Tier 9)",
+folder:"pets"
+},
+
+
 ];
